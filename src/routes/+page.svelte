5f78a2ee8f1f61cd08f04bdc249db347453d898b
Lynette Lopez <lynettelopez12@gmail.com>
2023-06-22 00:14:39 -0400

    Change color scheme of test page to neutral

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
   /* ... */
 </script>
 
-<div class="w-screen h-screen flex justify-center items-center bg-slate-50">
-  <h1 class="text-slate-800">Coming soon.</h1>
+<div class="w-screen h-screen flex justify-center items-center bg-neutral-50">
+  <h1 class="text-neutral-900">Coming soon.</h1>
 </div>
