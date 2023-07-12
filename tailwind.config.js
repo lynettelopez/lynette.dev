76a6dc78c623d634c2e790c8b0919ffb6c450563
Lynette Lopez <lynettelopez12@gmail.com>
2023-07-12 00:06:38 -0400

    Add color palette to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   plugins: [],
   theme: {
-    extend: {}
+    colors: {
+      // Utility
+      current: 'currentColor',
+      transparent: 'transparent',
+      // Primary
+      indigo: '#6320EE',
+      // Secondary
+      white: '#FFFFFF',
+      gray: {
+        light: '#F6F6F6',
+        dark: '#A9A9A9'
+      },
+      black: '#323232'
+    },
   }
 };
