817a5825a5da74688a35e137a2300b528ddf70f3
Lynette Lopez <lynettelopez12@gmail.com>
2023-07-20 10:28:01 -0400

    Create root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
-<script lang="ts">
-  /* ... */
-</script>
-
-<div class="w-screen h-screen flex justify-center items-center bg-neutral-50">
-  <h1 class="text-neutral-900">Coming soon.</h1>
+<div class="w-full h-full flex flex-row justify-end items-center">
+  <div class="w-4/5 space-y-32">
+    <h1 class="font-opti-champion text-black space-y-4">
+      <span class="block text-5xl animate-fade-right">Hey, web cowboy,</span>
+      <span class="inline-block text-9xl animate-fade-right">I'm Lynette</span>
+      <span class="inline-block text-9xl font-knewave text-indigo animate-fade-in animate-delay-500"
+        >*</span
+      >
+    </h1>
+    <p
+      class="w-2/4 font-nunito-sans font-light text-black text-2xl animate-fade-down animate-delay-700"
+    >
+      <span class="font-knewave text-indigo">*</span> A full-stack software engineer based in Rhode Island.
+      Obsessed with building beautiful, functional, and accessible web applications. Lover of coffee,
+      writing, learning new languages (both human and computer-based), and all things modern minimalism.
+    </p>
+  </div>
 </div>
