da38a6c1ee8f311b681651ad0e17bf527e1c8942
Lynette Lopez <lynettelopez12@gmail.com>
2023-07-20 23:51:41 -0400

    Move animate-* Tailwind utilities to end

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,17 +5,17 @@
 
 <div class="relative w-screen h-screen flex bg-gray-light">
   <div class="z-0 absolute w-full h-full grid grid-cols-4">
-    <div class="w-px h-0 col-start-2 animate-grow-down bg-gray-dark opacity-50" />
-    <div class="w-px h-0 col-start-3 animate-grow-down animate-delay-100 bg-gray-dark opacity-50" />
-    <div class="w-px h-0 col-start-4 animate-grow-down animate-delay-200 bg-gray-dark opacity-50" />
+    <div class="w-px h-0 col-start-2 bg-gray-dark opacity-50 animate-grow-down" />
+    <div class="w-px h-0 col-start-3 bg-gray-dark opacity-50 animate-grow-down animate-delay-100" />
+    <div class="w-px h-0 col-start-4 bg-gray-dark opacity-50 animate-grow-down animate-delay-200" />
   </div>
   <div class="z-0 absolute w-full h-full grid grid-rows-4">
-    <div class="w-0 h-px row-start-2 animate-grow-right bg-gray-dark opacity-50" />
+    <div class="w-0 h-px row-start-2 bg-gray-dark opacity-50 animate-grow-right" />
     <div
-      class="w-0 h-px row-start-3 animate-grow-right animate-delay-100 bg-gray-dark opacity-50"
+      class="w-0 h-px row-start-3 bg-gray-dark opacity-50 animate-grow-right animate-delay-100"
     />
     <div
-      class="w-0 h-px row-start-4 animate-grow-right animate-delay-200 bg-gray-dark opacity-50"
+      class="w-0 h-px row-start-4 bg-gray-dark opacity-50 animate-grow-right animate-delay-200"
     />
   </div>
   <div class="z-10 basis-5/6">
