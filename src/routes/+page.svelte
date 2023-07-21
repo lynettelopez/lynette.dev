d0ef829f2a5e7aed978633c9747ec25d5dd366ff
Lynette Lopez <lynettelopez12@gmail.com>
2023-07-21 00:11:29 -0400

    Increase responsiveness on smaller screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,18 @@
 <div class="w-full h-full flex justify-end items-center">
   <div class="w-4/5 space-y-32">
-    <h1 class="font-opti-champion text-black space-y-4">
-      <span class="block text-5xl animate-fade-right">Hey, web cowboy,</span>
-      <span class="inline-block text-9xl animate-fade-right">I'm Lynette</span>
-      <span class="inline-block text-9xl font-knewave text-indigo animate-fade-in animate-delay-500"
-        >*</span
+    <h1 class="space-y-4">
+      <span
+        class="block font-opti-champion text-black text-xl md:text-3xl xl:text-5xl animate-fade-right"
+        >Hey, web cowboy,</span
       >
+      <span
+        class="inline-block font-opti-champion text-black text-5xl md:text-7xl xl:text-9xl animate-fade-right"
+        >I'm Lynette
+        <span class="font-knewave text-indigo animate-fade-in animate-delay-500">*</span>
+      </span>
     </h1>
     <p
-      class="w-2/4 font-nunito-sans font-light text-black text-2xl animate-fade-down animate-delay-700"
+      class="w-2/4 max-h-60 overflow-auto sm:max-h-none sm:overflow-visible font-nunito-sans font-light text-black text-lg md:text-xl xl:text-2xl animate-fade-down animate-delay-700"
     >
       <span class="font-knewave text-indigo">*</span> A full-stack software engineer based in Rhode Island.
       Obsessed with building beautiful, functional, and accessible web applications. Lover of coffee,
